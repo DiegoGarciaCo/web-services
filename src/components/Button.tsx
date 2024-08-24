e1a9ee874879706507d0fbf116ec6bcf0c9ec965
Diego Garcia <diegogarcia51916@gmail.com>
2024-08-23 20:53:06 -0500

    feat: created button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+type buttonProps = {
+  buttonStyle: string;
+  buttonSize: string;
+  type: "submit" | "button";
+  onClick: MouseEvent;
+  text: string;
+};
+export default function Button({
+  buttonStyle,
+  buttonSize,
+  type,
+  onClick,
+  text,
+}: buttonProps) {
+  const STYLES: string[] = ["btn--primary", "btn--outline"];
+  const SIZE: string[] = ["btn--small", "btn--medium", "btn--large"];
+
+  const checkButtonStyle: string = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  const checkButtonSize: string = SIZE.includes(buttonSize)
+    ? buttonSize
+    : SIZE[1];
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      type={type}
+    >
+      {text}
+    </button>
+  );
+}
