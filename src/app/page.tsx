03303e2940081574dfc4629cc92b414cc3b5ca51
Diego Garcia <diegogarcia51916@gmail.com>
2024-08-27 10:54:35 -0500

    added hero section content on top of video background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
+import Button from "@/components/Button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-        autoPlay
-        loop
-        muted
-      >
-        <source src="/video-2.mp4" type="video/mp4" />
-      </video>
-      <h1>Hello World!</h1>
-    </main>
+    <>
+      <section className="flex min-h-screen flex-col items-center justify-between p-24">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+          autoPlay
+          loop
+          muted
+        >
+          <source src="/video-2.mp4" type="video/mp4" />
+        </video>
+        {/* hero content left aligned */}
+        <div className="flex flex-col items-start justify-center w-full text-white">
+          <h1 className="text-5xl font-bold">Web Services</h1>
+          <p className="text-xl mt-4">
+            Web sites Bots AI agents Android and IOS apps
+          </p>
+          <div className="flex mt-8 gap-4">
+            <Button
+              buttonSize="btn--medium"
+              buttonStyle="btn--primary"
+              type="button"
+              text="Get Started"
+              onClick
+            />
+            <Button
+              buttonSize="btn--medium"
+              buttonStyle="btn--outline"
+              type="button"
+              text="Contact"
+              onClick
+            />
+          </div>
+        </div>
+      </section>
+      <main>
+        <section className="min-w-full min-h-screen border-l-indigo-950"></section>
+      </main>
+    </>
   );
 }
