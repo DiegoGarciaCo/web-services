4d9db8540efd99925e32323f2a66d7c901756f2b
Diego Garcia <diegogarcia51916@gmail.com>
2024-08-27 09:51:46 -0500

    fix: made navbar transparent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function Navbar() {
   return (
-    <nav className="flex content-center justify-center sticky top-0 w-full h-20 bg-slate-500 items-center px-5">
+    <nav className="flex content-center justify-center sticky top-0 w-full h-20 bg-transparent items-center px-5">
       {/* left logo and links container */}
       <div className="flex space-x-5 text-white items-center w-3/4 justify-around">
         <h1 className="text-2xl px-5">Logo</h1>
