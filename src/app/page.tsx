494bee843ea4f7a274ee72518b203a7c58ed264a
Diego Garcia <diegogarcia51916@gmail.com>
2024-08-26 21:40:20 -0500

    added a video background to the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/video-2.mp4" type="video/mp4" />
+      </video>
       <h1>Hello World!</h1>
     </main>
   );
