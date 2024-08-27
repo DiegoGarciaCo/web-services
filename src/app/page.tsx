141e31d603af3d1857c089e1bbe8aef6a2064166
Diego Garcia <diegogarcia51916@gmail.com>
2024-08-27 17:32:45 -0500

    feat: created footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from "@/components/Button";
+import NavLinks from "@/components/NavLinks";
 import ProjectCards from "@/components/ProjectCards";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 export default function Home() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <section className="flex min-h-screen flex-col items-center justify-between p-24">
         <video
           className="absolute top-0 left-0 w-full h-full object-cover -z-10"
@@ -62,6 +64,28 @@ export default function Home() {
           </div>
         </section>
       </main>
-    </>
+      <footer className="h-[50vh] max-w-full text-white relative">
+        <div className="flex justify-evenly items-center p-5">
+          <h1 className="text-4xl cursor-pointer">LOGO</h1>
+          <NavLinks />
+          <div className="flex items-center">
+            <span className="social facebook">
+              <i className="fab fa-facebook-f"></i>
+            </span>
+            <span className="social twitter">
+              <i className="fab fa-twitter"></i>
+            </span>
+            <span className="social instagram">
+              <i className="fab fa-instagram"></i>
+            </span>
+          </div>
+        </div>
+        <div className="absolute bottom-0 p-3 border-t-2 border-white w-full">
+          <p className="text-center text-sm">
+            &copy; 2024 Web Services. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }
